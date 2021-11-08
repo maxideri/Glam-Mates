4d194242e22a83d5dd2097776ef32d9eecb07037
ramiporta <p0rt4csd@gmail.com>
2021-11-08 13:16:33 -0300

    Correccion ruta img productos

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ function mostrarProductos(){
             modalButton.setAttribute("type","button");
             modalButton.setAttribute("data-bs-toggle","modal")
             modalButton.setAttribute("data-bs-target",`#${tipo}${producto[0]}`) 
-            modalButton.setAttribute("style",`background-image: url(./../../${producto[2]}`)
+            modalButton.setAttribute("style",`background-image: url(../../${producto[2]})`)
             modalButton.innerHTML = `<img class="multiphoto" src="img/productos/multiphoto.svg" height="20px">`
             products.appendChild(modalButton)
 
@@ -96,7 +96,7 @@ function mostrarProductos(){
                 else {
                     foto.className = "producto-modal carousel-item"
                 }
-                foto.setAttribute("style",`background-image: url(./../../${producto[c]})`)
+                foto.setAttribute("style",`background-image: url(../../${producto[c]})`)
                 carousel.appendChild(foto)
             }
             i++;
